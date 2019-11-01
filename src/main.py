6a81d6bd43b5ef2db4ad5328c1e5ca2264955911
yuhouzhou <zyh.germany@gmail.com>
2019-11-01 16:22:16 +0100

    save finished work

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('../data/network_tf_gene.txt', sep='\t', header=None, comment='#').drop([3, 4, 5], axis=1)
+    df.columns = ['tf', 'gene', 'effect']
+
+    plt.rc('figure', figsize=(10, 6))
+    G = nx.from_pandas_edgelist(df=df, source='tf', target='gene', edge_attr='effect')
+
+    nx.draw(G, node_size=0.1)
+
+    plt.tight_layout()
+    plt.savefig('network.pdf')
+    plt.show()
